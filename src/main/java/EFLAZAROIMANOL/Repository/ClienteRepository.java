6fc1b34d84fd09295d6ed4788e2cfdd882c0d868
Lazro123 <lazaroimanol@gmail.com>
2022-08-16 22:17:06 -0500

    first commit

---
+++ src/main/java/EFLAZAROIMANOL/Repository/ClienteRepository.java
@@ -0,0 +1,10 @@
+package EFLAZAROIMANOL.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import EFLAZAROIMANOL.Model.Cliente;
+@Repository
+public interface ClienteRepository  extends JpaRepository<Cliente, Integer> {
+
+}
